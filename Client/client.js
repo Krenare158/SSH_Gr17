6b7a169652682e4a20797ba1b9a1687531977541
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-03-27 22:12:54 +0100

    client.js

--- Client/client.js
+++ Client/client.js
@@ -1,1 +1,11 @@
+import axios from 'axios';
 
+const SERVER_BASE_URL = 'http://localhost:3000'; 
+axios.post(`${SERVER_BASE_URL}/api/greet`, { name: 'John' })
+    .then(response => {
+        console.log('Server Response:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    export default axios;
