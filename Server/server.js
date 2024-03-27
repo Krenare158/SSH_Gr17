eb019e512732c2b9ed599b34e62f8edfa290bbe5
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-03-27 22:13:22 +0100

    server.js

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+
+app.post('/api/greet', (req, res) => {
+    const name = req.body.name;
+    res.send(`Hello, ${name}!`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
 
