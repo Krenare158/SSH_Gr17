0d1796030006d4730add62f2690423be039b3eb0
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-03-27 22:12:06 +0100

    App.js

--- Authentication/App.js
+++ Authentication/App.js
@@ -1,1 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+
+function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/register" />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    );
+}
+
+export default App;
 
