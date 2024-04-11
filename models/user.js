a533c8ca8f277ae8018f5206271e6598c5206f08
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2024-04-12 02:55:39 +0930

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,17 @@
+const users = sequelize.define("users", {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING, 
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+});
 
