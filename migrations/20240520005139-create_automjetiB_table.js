9e515d8be33c7ff91eff604f4967fe42f4ba16ff
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-05-20 04:08:58 +0200

    20240520005139-create_automjetiB_table.js

--- migrations/20240520005139-create_automjetiB_table.js
+++ migrations/20240520005139-create_automjetiB_table.js
@@ -1,22 +1,63 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('autob', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      vehicleType: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      ownerType: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      englishRegistration: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      mathsRegistration: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      personalNumber: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      registrationType: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      municipality: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      marka: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      engineCapacity: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('autob');
   }
 };
