315f0a16afeffa9f0e8754af18be19ffb32c4f53
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-05-20 04:07:35 +0200

    20240520004712-create_adresa_table.js

--- migrations/20240520004712-create_adresa_table.js
+++ migrations/20240520004712-create_adresa_table.js
@@ -1,22 +1,45 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('postal_addresses', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      street: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      country: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      postal_code: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('postal_addresses');
   }
 };
+
