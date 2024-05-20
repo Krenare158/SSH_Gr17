f8c24367eb1f4d6911b9087f02d96943feb24ce6
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-05-20 17:58:03 +0200

    20240520020115-creat_kru_table.js

---
+++ migrations/20240520020115-creat_kru_table.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('kru_water_bills', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      consumer_number: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('kru_water_bills');
+  }
+};
