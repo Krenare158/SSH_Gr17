f01ca347ee2e719d7057b13dc082e901772c9b8c
MajlindaHalilaj <121357662+MajlindaHalilaj@users.noreply.github.com>
2024-05-27 01:46:10 +0200

    changes

--- Client/Sherbimet/AuditoretLigjore/auditoretLigjore.js
+++ Client/Sherbimet/AuditoretLigjore/auditoretLigjore.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './auditoretLigjore.css';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faAddressCard} from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faMoneyBillAlt} from '@fortawesome/free-solid-svg-icons';
 
 const auditoretLigjore = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  
+  const handleLinkClick = (event) => {
+    event.preventDefault();
+    setShowAlert(true);
+  };
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
   return (
       <div className="auditoretLigjore-service-page">
         <div className="auditoretLigjore-service-section">
-          <h4 className="auditoretLigjore-section-title">Fondi i Sigurise</h4>
+          <h4 className="auditoretLigjore-section-title">Grante dhe Subvencione</h4>
           <div className="auditoretLigjore-options">
             <input type="radio" id="auditoretLigjoreRadio1" name="auditoretLigjoreOptions" value="option1" />
             <label htmlFor="auditoretLigjoreRadio1">Option 1</label>
@@ -27,21 +38,22 @@ const auditoretLigjore = () => {
         <div className="auditoretLigjore-service-cards-container">
           <div className="auditoretLigjore-service-cards-wrapper">
             <div className="auditoretLigjore-service-card">
-              <div className="auditoretLigjore-card-content">
-                <FontAwesomeIcon icon={faAddressCard} className="auditoretLigjore-card-icon" />
-                <h2>Aplikimi per firme te auditimeve ligjor per here te pare</h2>
-                <span className="auditoretLigjore-arrow">&#62;</span>
-              </div>
-            </div>
-            <div className="auditoretLigjore-service-card">
-              <div className="auditoretLigjore-card-content">
-                <FontAwesomeIcon icon={faAddressCard} className="auditoretLigjore-card-icon" />
-                <h2>Aplikimi per firme te auditimeve ligjor per pertrirje te licenses</h2>
-                <span className="auditoretLigjore-arrow">&#62;</span>
-              </div>
+            <div className="auditoretLigjore-card-content">
+                <FontAwesomeIcon icon={faMoneyBillAlt} className="auditoretLigjore-card-icon" />
+                <Link to="/AuditoretLigjore/PertrirjaLicenses" style={{ color: '#333' }} className='linkcla' onClick={handleLinkClick}>Aplikimi per firme te auditimeve ligjore per pertrirje te licenses</Link>
+                <span className="new-arrow">&#62;</span>
+             </div>
             </div>
           </div>
         </div>
+
+        {showAlert && (
+        <div className="custom-alert">
+          <h5>Njoftim</h5>
+          <p>Ju nuk mund te beni pertrirje te licenses per arsye se nuk keni ndonje firme te auditimeve ligjore te regjistruar.</p>
+          <button onClick={handleCloseAlert}>Close</button>
+        </div>
+      )}
       </div>
   );
 }
