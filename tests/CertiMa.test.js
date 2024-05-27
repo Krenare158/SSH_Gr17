269e747bbe51aafb5eab2192bbebfcde67a1327b
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2024-05-28 03:16:32 +0930

    Add files via upload

---
+++ tests/CertiMa.test.js
@@ -0,0 +1,17 @@
+// CertiMa.test.js
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import CertiMa from './CertiMa';
+
+test('submits form with correct data', () => {
+  render(<CertiMa />);
+  
+  // Mbushni dropdown-in me një anëtar të familjes
+  fireEvent.change(screen.getByLabelText(/Zgjedh anetarin/i), { target: { value: '1' } });
+
+  // Përçoni formën
+  fireEvent.click(screen.getByText(/Shkarko/i));
+
+  // Kontrolloni nëse kërkesa për shkarkim është thirrur me të dhënat e duhura
+  expect(console.log).toHaveBeenCalledWith("1");
+});
