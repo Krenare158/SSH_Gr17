b51b900ceef55f4cc5f7994e9d5e7734c6d43e2c
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2024-05-27 19:21:15 +0200

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -1,1 +1,21 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sisteme', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.AutoRegjistrim = require('../../models/AutoRegjistrim')(sequelize, DataTypes);
+db.AutoB = require('../../models/AutoB')(sequelize, DataTypes);
+db.AutoP = require('../../models/AutoP')(sequelize, DataTypes);
+db.ApLehonat = require('../../models/ApLehonat')(sequelize, DataTypes);
+db.ApFemije = require('../../models/ApFemije')(sequelize, DataTypes);
+db.CertiFam = require('../../models/CertiFam')(sequelize, DataTypes);
+db.AdPostare = require('../../models/AdPostare')(sequelize, DataTypes);
+db.Deklarimi = require('../../models/Deklarimi')(sequelize, DataTypes);
+db.Vula = require('../../models/Vula')(sequelize, DataTypes);
+
+module.exports = db;
