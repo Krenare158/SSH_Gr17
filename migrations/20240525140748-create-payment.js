9c6be59a7c33c2e0df8081cd892cccbbfe58b165
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2024-05-27 19:13:04 +0200

    Add files via upload

---
+++ migrations/20240525140748-create-payment.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Payments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cardHolderName: {
+        type: Sequelize.STRING
+      },
+      cardNumber: {
+        type: Sequelize.STRING
+      },
+      expiryDate: {
+        type: Sequelize.STRING
+      },
+      cvv: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Payments');
+  }
+};
